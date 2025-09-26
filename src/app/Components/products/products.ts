0861857bbc8caf847ac121e9d6eb91e730fd198f
mostafa2ahmedev <mostafaahmedev2002@gmail.com>
2025-09-26 17:59:49 +0300

    Binding

---
+++ src/app/Components/products/products.ts
@@ -0,0 +1,103 @@
+import { Component } from '@angular/core';
+import { Iproduct } from '../../Models/iproduct';
+import { CommonModule } from '@angular/common';
+import { Icategory } from '../../Models/icategory';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-products',
+  imports: [CommonModule, FormsModule],
+  templateUrl: './products.html',
+  styleUrl: './products.css',
+})
+export class Products {
+  products: Iproduct[];
+  categories: Icategory[];
+  seletctedCategoryId: number = 0;
+  total: number = 0;
+  constructor() {
+    this.products = [
+      {
+        id: 100,
+        name: 'Dell Laptop',
+        price: 50000,
+        quantity: 3,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 1,
+      },
+      {
+        id: 200,
+        name: 'Assus Laptop',
+        price: 50000,
+        quantity: 0,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 1,
+      },
+      {
+        id: 300,
+        name: 'Lenovo Laptop',
+        price: 50000,
+        quantity: 3,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 1,
+      },
+      {
+        id: 400,
+        name: 'Iphone',
+        price: 50000,
+        quantity: 3,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 3,
+      },
+      {
+        id: 500,
+        name: 'MSI Laptop',
+        price: 50000,
+        quantity: 3,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 1,
+      },
+      {
+        id: 600,
+        name: 'Watch ',
+        price: 50000,
+        quantity: 3,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 2,
+      },
+      {
+        id: 700,
+        name: 'AirPods',
+        price: 50000,
+        quantity: 4,
+        imgUrl: 'https://picsum.photos/seed/picsum/200/300',
+        catId: 4,
+      },
+    ];
+    this.categories = [
+      {
+        id: 1,
+        name: 'Laptop',
+      },
+      {
+        id: 2,
+        name: 'Mobile',
+      },
+      {
+        id: 3,
+        name: 'Watch',
+      },
+      {
+        id: 4,
+        name: 'Accessories',
+      },
+    ];
+  }
+
+  buy(count: string, price: number) {
+    this.total += +count * price; //casting
+  }
+  change() {
+    this.seletctedCategoryId = 3;
+  }
+}
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Products } from "./Components/products/products";
+import { Footer } from "./Components/footer/footer";
+import { Header } from "./Components/header/header";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Products, Footer, Header],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
+  standalone: true,
 })
 export class App {
   protected readonly title = signal('ecommerceApp');
